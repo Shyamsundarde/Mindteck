50fd16ce2422503f07f1282f40d9163d9360f10c
Shyamsundarde <111341214+Shyamsundarde@users.noreply.github.com>
2022-08-16 16:04:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Mindteck";
+            string rev = "";
+           int len = word.Length;
+            for (int i = len - 1; i >= 0; i--)
+            {
+                rev=rev+word[i];
+            }
+            Console.WriteLine(rev);
+        }
+    }
+}
+
