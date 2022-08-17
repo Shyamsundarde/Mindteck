41831de179396cb75b49fb0838a59a3acb208af6
Shyamsundarde <111341214+Shyamsundarde@users.noreply.github.com>
2022-08-17 16:04:31 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,19 @@
 ﻿using System;
-namespace myconsoleapplication
+namespace ConsoleApp1
 {
-    class shyam
+    class Program
     {
-        public delegate void Shyamdeligate(string name);
-        public delegate void Multideligate(int x, int y);
-        public void Multi(int x, int y)
-        {
-            Console.WriteLine(x * y);
-
-        }
-        public void name(string name)
-        {
-            Console.WriteLine("my name is" + " " + name);
-
-        }
         static void Main(string[] args)
         {
-            shyam d = new shyam();
-            Shyamdeligate obj1 = new Shyamdeligate(d.name);
-            Multideligate obj2 = new Multideligate(d.Multi);
-            obj1("Shyam");
-            obj2(10, 20);
-
+            string word = "Mindteck";
+            string rev = "";
+           int len = word.Length;
+            for (int i = len - 1; i >= 0; i--)
+            {
+                rev=rev+word[i];
+            }
+            Console.WriteLine(rev);
         }
-
     }
 }
 
-
-    //Exception
