7d31d676f0953a4c015e03dbb1ce71c356217665
Shyamsundarde <111341214+Shyamsundarde@users.noreply.github.com>
2022-08-17 16:05:01 +0530

    Add files via upload

---
+++ Class4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+
+    class shyam
+    {
+        public delegate void Shyamdeligate(string name);
+        public delegate void Multideligate(int x, int y);
+        public void Multi(int x, int y)
+        {
+            Console.WriteLine(x * y);
+
+        }
+        public void name(string name)
+        {
+            Console.WriteLine("my name is" + " " + name);
+
+        }
+        static void Main(string[] args)
+        {
+            shyam d = new shyam();
+            Shyamdeligate obj1 = new Shyamdeligate(d.name);
+            Multideligate obj2 = new Multideligate(d.Multi);
+            obj1("Shyam");
+            obj2(10, 20);
+
+        }
+
+    }
+
+}
