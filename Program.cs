f8ebb0a1855a06209f5f8533d117e479735d9e5d
Shyamsundarde <111341214+Shyamsundarde@users.noreply.github.com>
2022-08-17 15:52:40 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,19 @@
 ﻿using System;
-using System.Security.Claims;
-
-namespace ConsoleApp2
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<Class1>student = new List<Class1>()
+            string word = "Mindteck";
+            string rev = "";
+           int len = word.Length;
+            for (int i = len - 1; i >= 0; i--)
             {
-                new Class1() {studentId=1,Name="Shyam",Dept="ECE",Dob="21.05.2000"},
-                new Class1() {studentId=2,Name="Abhishek",Dept="EE",Dob="24.01.2001"},
-                new Class1() {studentId=3,Name="Subhojit",Dept="IT",Dob="18.09.1999"},
-                new Class1() {studentId=4,Name="Sohan",Dept="ECE",Dob="10.11.1997"}
-            };
-
-            var depList = from dlst in student
-                          where dlst.Dept == "ECE"
-                          select dlst;
-
-            foreach(var dep in depList)
-                Console.WriteLine(dep.studentId + "-" + dep.Name + "......" + "ECE" + "-" + dep.Dob);
+                rev=rev+word[i];
+            }
+            Console.WriteLine(rev);
         }
-    }
-
-    public class Class1
-    {
-        public int studentId { get; set;}
-        public string Name { get; set;}
-        public string Dept { get; set;}
-
-        public string Dob { get; set;}
     }
 }
 
