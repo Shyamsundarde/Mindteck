3813b3338b2ec5bc81910808bbcc8b5380b9f34c
Shyamsundarde <111341214+Shyamsundarde@users.noreply.github.com>
2022-08-18 10:22:57 +0530

    Add files via upload

---
+++ Object creating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class fruits
+{
+    string Fruit_Name;
+    public void Quality()
+    {
+        Console.WriteLine("Food is so delicious");
+    }
+    public void Price(int x)
+    {
+        Console.WriteLine("The price of the Fruit is" + "" + " " + x);
+    }
+    static void Main(string[] args)
+    {
+        fruits v = new fruits();
+        v.Fruit_Name = "Mango";
+        v.Quality();
+        v.Price(120);
+        Console.WriteLine(v.Fruit_Name = "Mango");
+        Console.ReadLine();
+
+
+    }
+}
