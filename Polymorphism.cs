68c49e14b03d4c3957e3cd4d2b5676fbb9d1fea4
Shyamsundarde <111341214+Shyamsundarde@users.noreply.github.com>
2022-08-18 16:00:15 +0530

    Add files via upload

---
+++ Polymorphism.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSPROJECT
+{
+    class polymorphisom
+    {
+        public class property
+        {
+            public virtual void phone()
+            {
+                Console.WriteLine("only phone is needed");
+            }
+        }
+        public class me : property
+        {
+            public override void phone()
+            {
+
+                Console.WriteLine("Money is also need");
+            }
+            static void Main()
+            {
+                me p = new me();
+                p.phone();
+            }
+
+        }
+    }
+}
